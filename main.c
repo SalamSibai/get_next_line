08d98250483363284456fd75c0efd247a9d9c366
Salam Sibai <salamsibai96@gmail.com>
2024-02-03 15:16:10 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "get_next_line.h"
+/*#include "get_next_line.h"
 
 int main()
 {
@@ -8,4 +8,4 @@ int main()
 	printf("%s\n", get_next_line(fd));
 	printf("%s\n", get_next_line(fd));
 	return 0;
-}
+}*/
